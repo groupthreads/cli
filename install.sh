f3c268eba9f5e655a603413eb30e0f3cf995d410
Jam Risser <jam@jamrizzi.com>
2017-01-25 19:48:21 -0600

    Renamed alias to gt

--- install.sh
+++ install.sh
@@ -14,4 +14,4 @@ chown -R $SUDO_USER:$SUDO_USER /opt/groupthreads-cli/
 runuser -l $SUDO_USER -c 'git clone https://github.com/groupthreads/cli.git /opt/groupthreads-cli/'
 chown -R root:root /opt/groupthreads-cli/
 chmod +x /opt/groupthreads-cli/groupthreads.py
-runuser -l $SUDO_USER -c 'echo "alias groupthreads=\"python /opt/groupthreads-cli/groupthreads.py\"" | tee -a ~/.'"$SHELL_CONFIG"
+runuser -l $SUDO_USER -c 'echo "alias gt=\"python /opt/groupthreads-cli/groupthreads.py\"" | tee -a ~/.'"$SHELL_CONFIG"
