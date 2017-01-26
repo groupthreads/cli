9fcbefbb049fd0c7a66cc51f0e33c4960370997a
Jam Risser <jam@jamrizzi.com>
2017-01-25 19:14:45 -0600

    Created installer

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cd /opt
-git clone https://github.com/groupthreads/cli.git
-chmod +x /opt/cli/groupthreads.py
-echo "alias groupthreads=\"python /opt/groupthreads.py\""
+SHELL_CONFIG=bashrc
+CURRENT_SHELL=$(runuser -l $SUDO_USER -c 'echo $SHELL')
+
+if [ "$CURRENT_SHELL" == "/bin/zsh" ]; then
+    SHELL_CONFIG=zshrc
+fi
+
+echo "Using shell $SHELL_CONFIG"
+
+mkdir /opt/groupthreads-cli/
+chown -R $SUDO_USER:$SUDO_USER /opt/groupthreads-cli/
+runuser -l $SUDO_USER -c 'git clone git@github.com:groupthreads/cli.git /opt/groupthreads-cli/'
+chown -R root:root /opt/groupthreads-cli/
+chmod +x /opt/groupthreads-cli/groupthreads.py
+runuser -l $SUDO_USER -c 'echo "alias groupthreads=\"python /opt/groupthreads-cli/groupthreads.py\"" | tee -a ~/.'"$SHELL_CONFIG"
